1dd3111e99f47d15b934e428064c0d732b4a3645
Lê Khắc Thiện <muzankibut112@gmail.com>
2024-01-16 10:12:16 +0700

    Update UI Home Page

--- src/components/Pages/HR/HomeHR/HomeHR.js
+++ src/components/Pages/HR/HomeHR/HomeHR.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectUserData } from '../../../../redux/reducers/user'
-import * as EmployeeService from '../../../../services/EmployeeService'
 function HomeHR() {
     const userData = useSelector(selectUserData);
 
